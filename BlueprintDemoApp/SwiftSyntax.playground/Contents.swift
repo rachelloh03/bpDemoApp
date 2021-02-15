f01a4a96b045a52d0da31bfb1f3094503f10a34d
Mindy Long <mindyflong@gmail.com>
2021-02-15 17:13:46 -0500

    more documentation

--- BlueprintDemoApp/SwiftSyntax.playground/Contents.swift
+++ BlueprintDemoApp/SwiftSyntax.playground/Contents.swift
@@ -26,52 +26,35 @@ var var8: String
 
 // TODO: document some useful methods for each (e.g. arrays: append, count, reduce, map, remove, concat, etc.)
 
-// Tuples
+// Tuples //
 let myTuple = ("blueprint", 2021)
 
-//Tuple Methods
-myTuple[0]  //access element at index
+/* Tuple Methods */
+myTuple[0]  // access element at index
 
-// Arrays
+// Arrays //
 let myFirstArray = ["blueprint", 2021] // ERROR
 var mySecondArray: [Any] = ["blueprint", 2021]
 let emptyArray: [Int] = []
 let emptyArray2: Array<Double> = Array() // similar to Java
 let digitCounts = Array(repeating: 0, count: 10) //intializes array of 0s with length 10
 
-//Array Methods
-mySecondArray.isEmpty  //checks if empty
-mySecondArray.count  //length of array
-mySecondArray.first  //get first element of array
-mySecondArray.last  //get last element of array
-mySecondArray.append("new element") //append item to end of array
-mySecondArray.append(contentsOf: ["a", "b"]) //append multiple elements to end of array
-mySecondArray.insert("Swift", at: 1) //insert item at specified index
-mySecondArray.remove(at: 1) //removes element at index
-mySecondArray.removeLast() //remove last element
-mySecondArray[0] //get element at index 0
-mySecondArray[0...2] //gets subarray of elements, inclusive
+/* Array Methods */
+mySecondArray.isEmpty // checks if empty
+mySecondArray.count   // length of array
+mySecondArray.first   // get first element of array
+mySecondArray.last    // get last element of array
+mySecondArray.append("new element") // appends item to end of array
+mySecondArray.append(contentsOf: ["a", "b"]) // appends multiple elements to end of array
+mySecondArray.insert("Swift", at: 1) // inserts item at specified index
+mySecondArray.remove(at: 1) // removes element at index
+mySecondArray.removeLast()  // remove last element
+mySecondArray[0]            // get element at index 0
+mySecondArray[0...2]        // gets subarray of elements, inclusive
+emptyArray2.reduce(0, { x, y in x + y }) // produces a single value from the elements of an entire sequence
+emptyArray.map{$0 * 10}                  // applies the function to each element ($0) in the array
 
-
-// Sets- used to test efficiently for membership when order doesn't matter
-let mySet: Set = ["start hacking", "mobile dev (iOS)", "mobile dev (Android)", "web dev beginner", "web dev advanced"]
-let set2: Set = ["start hacking", "a", "Swift", "test"]
-
-// Set methods
-mySet.contains("test") //tes
-mySet.isSubset(of: set2)  //test if your set is a subset
-mySet.union(set2) //join with another set
-mySet.intersection(set2) //get intersection of this set with other set
-// can also use any nonmutating sequence or collection methods with a set
-
-// Dictionaries
-let myDict: Dictionary<String, Int> = [:]
-let myDict2 = [Int: UILabel]()
-
-// Typealiases: alternate name for existing type
-typealias CustomType = [UUID:People] // assume we have a class People
-
-// Iterating
+/* Iterating */
 for i in 0...mySecondArray.count {
     print(mySecondArray[i])
 }
@@ -80,15 +63,58 @@ for element in mySecondArray {
     print(element)
 }
 
+
+/* Other Array Methods */
+/*
+ concatentation: a = b + c
+ flatmap: removes all nil's from array
+ filter: removes all elements satisfying a condition
+ */
+
+// Sets //
+// Sets are used to test efficiently for membership when order doesn't matter. Think of it as hashing but without the mapping part.
+let mySet: Set = ["start hacking", "mobile dev (iOS)", "mobile dev (Android)", "web dev beginner", "web dev advanced"]
+let set2: Set = ["start hacking", "a", "Swift", "test"]
+
+/* Set methods */
+mySet.contains("test")    // does your set contain an element
+mySet.isSubset(of: set2)  // test if your set is a subset
+mySet.union(set2)         // join with another set
+mySet.intersection(set2)  // get intersection of this set with other set
+
+// You can also use any nonmutating sequence or collection methods with a set
+
+
+// Dictionaries //
+// Dictionaries are hashmaps. They can map one element to another.
+// EX: What if I wanted to store an array of names for each class? In this case, the class (name or id) would be a key and would point to an array of student names stored somewhere. Typing myDict[classname] would allow you to access that class.
+
+let myDict: Dictionary<String, Int> = [:]
+let myDict2 = [Int: UILabel]()
+
+// EX (continued)
+var myClass = [
+    "mobile dev (iOS)": ["Mindy", "Katherine", "Nat"],
+    "mobile dev (Android)": ["Arun", "Darren", "Anna", "Angela", "Vishruti"],
+    "web dev beginner": ["Jianna", "Justin"],
+    "web dev advanced": ["weblab"]
+]
+let instructors = myClass["mobile dev (iOS)"] // instructors = ["Arun", "Darren", "Anna", "Angela", "Vishruti"]
+
+
+// Typealiases: alternate name for existing type //
+typealias CustomType = [UUID:People] // Assume we have a class or struct People
+
+
 // ********* //
 // Optionals //
 // ********* //
 
-// Optional- is either an unwrapped value or nil- basically will either have a value that can be unwrapped and accessed, or not value
-// used in situations when a value may not exist- like when an operation may fail or return nothing
-// default is nil
-var myImage: UIImage? // same as `var myImage: UIImage? = nil`
-var mySlider: UISlider!
+// An optional is either an unwrapped value or nil. That means that it will either have a value that can be unwrapped and accessed, or be null.
+// Optionals are used in situations when a value may not exist - like when an operation may fail or return nothing
+// By default they are set to nil
+var myImage: UIImage?   // ? is the same as `var myImage: UIImage? = nil`
+var mySlider: UISlider! // ! is a forced unwrap operator. If the value is nil (not by default), the program will be stopped (unless you catch it)
 
 // Unwrapping optionals
 var unwrappedImage = UIImage()
@@ -147,16 +173,12 @@ do {
 // Enums, classes, & structs //
 // ************************* //
 
-// enum
+/* Enum: Think of this as a dropdown menu of options */
 enum Track {
     case startHacking, mobileiOS, mobileAndroid, webdevBeginner, webdevAdvanced
 }
 
-// both class and struct types can define properties, methods, initializers, and conform to protocols
-// Classes can inherit from each other and is a reference type- instances of a class are accessed through references
-// Struct is a value type and (kinda) immutable
-
-// struct
+/* Struct */
 struct Hacker {
     let name: String
     let id: Int
@@ -169,7 +191,7 @@ struct Hacker {
     }
 }
 
-// class
+/* Class */
 class Hackathon {
     let name: String = ""
     let location: String = ""
@@ -186,6 +208,41 @@ class Hackathon {
     }
 }
 
+
+// Both class and struct types can define properties, methods, initializers, and conform to protocols.
+// Classes can inherit from each other and is a reference type - instances of a class are accessed through references.
+// Struct is a value type and (kinda) immutable. Each instance of a struct is independent.
+
+/* CONCEPT CHECK
+ 
+ class X {
+ 
+    private var name: String?
+    
+    func setName(n: String) {
+        name = n
+    }
+    
+    func getName() -> String {
+        return name
+    }
+ 
+ }
+ 
+ let a = X()
+ a.setName("hi")
+ let b = a
+ a.setName("bye")
+ 
+ // What will b's name be?
+ b.getName() -> "bye"
+ 
+ // What if X were a struct?
+ b.getName() -> "hi"
+ 
+*/
+
+/* EXAMPLE USE */
 let hacker = Hacker(name: "Mindy", id: 1, track: .mobileiOS)
 let hackathon = Hackathon()
 hackathon.updateParticipants(hackers: [hacker])
