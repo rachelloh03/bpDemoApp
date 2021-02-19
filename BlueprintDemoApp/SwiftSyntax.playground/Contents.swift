52d51684a84c696f7ebc0192bc34eee6f8950dbe
Mindy Long <mindyflong@gmail.com>
2021-02-19 18:56:04 -0500

    updated syntax file

--- BlueprintDemoApp/SwiftSyntax.playground/Contents.swift
+++ BlueprintDemoApp/SwiftSyntax.playground/Contents.swift
@@ -4,11 +4,16 @@ import UIKit
 // Declaring variables
 // ******************* //
 
-let var1 = 5
-let var2 = "blueprint"
+
 // **** SYNTAX **** //
+
+// Static typing is when the user explicitly defines the type of the variable at declaration.
 // let constant name: type = expression
 let var4: Float = 1.0
+
+// Inferred typing is when the editor interprets what type the variable is.
+let var1 = 5
+let var2 = "blueprint"
 
 // 'let' indicates a constant, so var5 must be set to a value at declaration
 let var5: UILabel // ERROR
@@ -24,14 +29,11 @@ var var8: String
 // Data Types //
 // ********** //
 
-// TODO: document some useful methods for each (e.g. arrays: append, count, reduce, map, remove, concat, etc.)
-
 // Tuples //
 let myTuple = ("blueprint", 2021)
 
 /* Tuple Methods */
 myTuple[0]  // access element at index
-
 // Arrays //
 let myFirstArray = ["blueprint", 2021] // ERROR
 var mySecondArray: [Any] = ["blueprint", 2021]
@@ -53,7 +55,6 @@ mySecondArray[0]            // get element at index 0
 mySecondArray[0...2]        // gets subarray of elements, inclusive
 emptyArray2.reduce(0, { x, y in x + y }) // produces a single value from the elements of an entire sequence
 emptyArray.map{$0 * 10}                  // applies the function to each element ($0) in the array
-
 /* Iterating */
 for i in 0...mySecondArray.count {
     print(mySecondArray[i])
@@ -62,7 +63,6 @@ for i in 0...mySecondArray.count {
 for element in mySecondArray {
     print(element)
 }
-
 
 /* Other Array Methods */
 /*
@@ -81,14 +81,11 @@ mySet.contains("test")    // does your set contain an element
 mySet.isSubset(of: set2)  // test if your set is a subset
 mySet.union(set2)         // join with another set
 mySet.intersection(set2)  // get intersection of this set with other set
-
 // You can also use any nonmutating sequence or collection methods with a set
-
 
 // Dictionaries //
 // Dictionaries are hashmaps. They can map one element to another.
 // EX: What if I wanted to store an array of names for each class? In this case, the class (name or id) would be a key and would point to an array of student names stored somewhere. Typing myDict[classname] would allow you to access that class.
-
 let myDict: Dictionary<String, Int> = [:]
 let myDict2 = [Int: UILabel]()
 
@@ -101,7 +98,6 @@ var myClass = [
 ]
 let instructors = myClass["mobile dev (iOS)"] // instructors = ["Arun", "Darren", "Anna", "Angela", "Vishruti"]
 
-
 // Typealiases: alternate name for existing type //
 typealias CustomType = [UUID:People] // Assume we have a class or struct People
 
@@ -109,12 +105,13 @@ typealias CustomType = [UUID:People] // Assume we have a class or struct People
 // ********* //
 // Optionals //
 // ********* //
-
-// An optional is either an unwrapped value or nil. That means that it will either have a value that can be unwrapped and accessed, or be null.
+// An optional is either an unwrapped value or nil. That means that it will either have a value or be null.
+// How do we access a value that can be nil though? This is called *unwrapping*.
 // Optionals are used in situations when a value may not exist - like when an operation may fail or return nothing
 // By default they are set to nil
+
 var myImage: UIImage?   // ? is the same as `var myImage: UIImage? = nil`
-var mySlider: UISlider! // ! is a forced unwrap operator. If the value is nil (not by default), the program will be stopped (unless you catch it)
+var mySlider: UISlider! // ! is the force unwrap operator. If the value is nil (not by default), the program will be terminated during runtime (unless you catch it)
 
 // Unwrapping optionals
 var unwrappedImage = UIImage()
@@ -144,7 +141,6 @@ func unwrapper() {
 // *************** //
 // Basic functions //
 // *************** //
-
 // func name(parameters) -> (return type) { function }
 func myVar(param: String) -> String {
     var v: String
@@ -158,7 +154,6 @@ print(myVar(param: var2))
 // ************** //
 // Error Handling //
 // ************** //
-
 func checkVar(v: String) throws {
     //..
 }
@@ -169,10 +164,10 @@ do {
     print("Something went wrong!")
 }
 
+
 // ************************* //
 // Enums, classes, & structs //
 // ************************* //
-
 /* Enum: Think of this as a dropdown menu of options */
 enum Track {
     case startHacking, mobileiOS, mobileAndroid, webdevBeginner, webdevAdvanced
@@ -185,7 +180,7 @@ struct Hacker {
     let track: Track
     var isCheckedIn: Bool = false
     
-    // mutating functions changes the values of the struct- internally, the old Hacker is replaced by a new Hacker with an updated isCheckedIn value
+    // Mutating functions changes the values of the struct- internally, the old Hacker is replaced by a new Hacker with a different isCheckedIn value.
     mutating func checkIn() {
         isCheckedIn = true
     }
@@ -211,8 +206,7 @@ class Hackathon {
 
 // Both class and struct types can define properties, methods, initializers, and conform to protocols.
 // Classes can inherit from each other and is a reference type - instances of a class are accessed through references.
-// Struct is a value type and (kinda) immutable. Each instance of a struct is independent.
-
+// Struct is a value type and, in a way, immutable. Each instance of a struct is independent of another.
 /* CONCEPT CHECK
  
  class X {
